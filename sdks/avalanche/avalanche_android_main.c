8d63450cc92d6b065255b7ddc1ce6e6116bdc1f5
PedroHLC <pedro.laracampos@gmail.com>
2014-01-31 22:54:29 -0200

    Now playable on android

---
+++ sdks/avalanche/avalanche_android_main.c
@@ -0,0 +1,27 @@
+/* For when compiling for ANDROID
+    Made from "SDL_android_main.c" */
+
+#include <SDL2/SDL_main.h>
+#include <jni.h>
+
+/* Called before SDL_main() to initialize JNI bindings in SDL library */
+extern void SDL_Android_Init(JNIEnv* env, jclass cls);
+
+/* Start up the SDL app */
+void Java_org_libsdl_app_SDLActivity_nativeInit(JNIEnv* env, jclass cls, jobject obj)
+{
+    /* This interface could expand with ABI negotiation, calbacks, etc. */
+    SDL_Android_Init(env, cls);
+
+    SDL_SetMainReady();
+
+    /* Run the application code! */
+    int status;
+    char *argv[2];
+    argv[0] = SDL_strdup("SDL_app");
+    argv[1] = NULL;
+    status = main(1, argv);
+
+    /* Do not issue an exit or the whole application will terminate instead of just the SDL thread */
+    /* exit(status); */
+}
