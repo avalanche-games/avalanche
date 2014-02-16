7b2bc7d2bcbdd333387d784fe52c4cca6400b3c0
PedroHLC <pedro.laracampos@gmail.com>
2014-02-16 20:53:33 -0300

    Fixed android auto build script

---
+++ examples/shooter/android_project/gen/R.java.d
@@ -0,0 +1,14 @@
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/gen/avalanche/shooterdemo/R.java \
+ : /home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values-v14/styles.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values-sw720dp-land/dimens.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/layout/activity_main.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/drawable-mdpi/ic_launcher.png \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values/strings.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values/dimens.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values/styles.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/drawable-hdpi/ic_launcher.png \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values-v11/styles.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/menu/main.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/values-sw600dp/dimens.xml \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/res/drawable-xhdpi/ic_launcher.png \
+/home/pedrohlc/Projects/avalanche/examples/shooter/android_project/bin/AndroidManifest.xml \
